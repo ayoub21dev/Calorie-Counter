0a51c66e4893649393b8500cdf0534267b60ae43
Ayoub jalyta <ayoubcode941@gmail.com>
2025-08-05 16:29:53 +0100

    Add calorie calculation for dinner, snacks, and budget inputs in calculateCalories function

--- script.js
+++ script.js
@@ -55,9 +55,15 @@ function calculateCalories(e) {
   const snacksNumberInputs = document.querySelectorAll("#snacks input[type='number']");
   const exerciseNumberInputs = document.querySelectorAll("#exercise input[type='number']");
  
-    const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+  const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
   const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
   const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
   const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
-  const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
-}
+  const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+  if (isError) {
+    return
+} 
+
+  const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+  const remainingCalories = budgetCalories -consumedCalories +exerciseCalories
+} 
